ba60dd3fc555e874d0789919c68b3ac932623a77
colurw <colurw@gmail.com>
2023-09-28 18:17:25 +0100

    nearest station/airport added

--- 1_data_cleaning.py
+++ 1_data_cleaning.py
@@ -1,16 +1,18 @@
 
-""" data: https://www.gov.uk/government/publications/civil-marriages-and-partnerships-approved-premises-list """
-"""       https://osdatahub.os.uk/downloads/open/CodePointOpen  """
+""" data: https://www.gov.uk/government/publications/civil-marriages-and-partnerships-approved-premises-list 
+          https://osdatahub.os.uk/downloads/open/CodePointOpen  
+          https://github.com/ellcom/UK-Train-Station-Locations """
 
 
 from pandas_ods_reader import read_ods
 import pickle
 import csv
 from convertbng.util import convert_lonlat
+from math import radians, cos, sin, acos
 
 
 def postcode_lat_lon(postcode):
-    """ get exact latitude and lontitude of a postcode using OS Code-Point data """
+    """ get exact latitude and longitude of a postcode using OS Code-Point data """
     letters = ''
     northing, easting = None, None
     # get first letters of postcode
@@ -20,7 +22,7 @@ def postcode_lat_lon(postcode):
         letters = letters + char         
     try:
         # find correct csv file
-        with open(f'codepo_gb/CSV/{letters}.csv', 'r') as csv_file:
+        with open(f'data/codepo_gb/CSV/{letters}.csv', 'r') as csv_file:
             reader = csv.reader(csv_file)  
             # lookup OSGB36 coordinates  
             for row in reader:
@@ -28,11 +30,59 @@ def postcode_lat_lon(postcode):
                     northing, easting = row[3], row[2]
     except:
         northing, easting = None, None
-    # convert OSGB36 coordinates to latitude and lontitude
+    # convert OSGB36 coordinates to latitude and longitude
     lon, lat = [None], [None]
     if northing and easting:
-        lon, lat = convert_lonlat([int(easting)],[int(northing)])    
+        lon, lat = convert_lonlat([int(easting)],[int(northing)])   
     return *lat, *lon
+
+
+def distance(lat1, lon1, lat2, lon2):
+    """ returns distance between two points in miles using haversine formula"""
+    try:
+        lat1 = radians(float(lat1))
+        lon1 = radians(float(lon1)) 
+        lat2 = radians(float(lat2)) 
+        lon2 = radians(float(lon2))
+        distance = 3963.0 * acos((sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(lon2-lon1))
+    except:
+        distance = 1000
+    return distance
+
+
+def dist_to_station(postcode):
+    """ returns distance from a postcode to nearest train station in miles """
+    max_dist = 100
+    # get coordinates of target postcode
+    lat1, lon1 = postcode_lat_lon(postcode)
+    # iterate through coordinates of stations
+    with open('data/uk_train_stations.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)  
+        for row in reader:
+            lat2, lon2 = row[2], row[3]
+            # record least distance to target postcode
+            dist = distance(lat1, lon1, lat2, lon2)
+            if dist < max_dist:
+                max_dist = dist
+    return int(max_dist)
+
+
+def dist_to_airport(postcode):
+    """ returns distance from a postcode to nearest airport in miles """
+    max_dist = 200
+    # get coordinates of target postcode
+    lat1, lon1 = postcode_lat_lon(postcode)
+    # iterate through postcodes of airports
+    with open('data/airport_postcodes.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)  
+        for row in reader:
+            # calculate coordinates of airport
+            lat2, lon2 = postcode_lat_lon(row[0])
+            # record least distance to target postcode
+            dist = distance(lat1, lon1, lat2, lon2)
+            if dist < max_dist:
+                max_dist = dist
+    return int(max_dist)
 
 
 # read and tidy data
@@ -56,15 +106,21 @@ df_venues['postcode'] = df_venues.apply(lambda row: ' '.join(row.address.split()
 df_venues['address'] = df_venues.apply(lambda row: ' '.join(row.address.split()[:-2]), axis = 1)
 df_venues = df_venues[['name', 'address', 'postcode', 'phone']]
 
-# add latitude and lontitude columns using Code-Point data
+# add venue latitude and longitude
 df_venues["latitude"] = df_venues["postcode"].apply(lambda row: postcode_lat_lon(row)[0])
 df_venues["longitude"] = df_venues["postcode"].apply(lambda row: postcode_lat_lon(row)[1])
+
+# drop rows with null lat/lon data
+df_venues = df_venues.dropna(subset=['latitude'])
+
+# add distances to nearest airport and station
+df_venues["station"] = df_venues["postcode"].apply(lambda row: dist_to_station(row))
+df_venues["airport"] = df_venues["postcode"].apply(lambda row: dist_to_airport(row))
 
 # remove index digits from name column
 df_venues["name"] = df_venues["name"].apply(lambda row: ''.join(filter(lambda char: not char.isdigit(), row)))
 
 # check dataframe
-print(df_venues.head(10))
 print(df_venues.tail(10))
 
 # save dataframe
--- 2_draw_map.py
+++ 2_draw_map.py
@@ -5,16 +5,25 @@ import pickle
 with open('pickled/df_venues.pkl', 'rb') as file:
      df_venues = pickle.load(file)
 
-# filter dataframe according to search term
-search_term = input("\n >>> Enter search term, or press ENTER for all: ").upper()
-df_filtered = df_venues[df_venues["name"].str.contains(search_term)]
+# search parameters
+SEARCH_TERMS = "farm|meadow| field|glebe|lea|marsh|park|tipi|tepee|canvas|yurt|holiday|festival|camp|outdoor|glamp"
+DROP_TERMS = "holiday inn|hotel|golf"
+MAX_DIST_TO_STATION = 10
+MAX_DIST_TO_AIRPORT = 50
+
+# filter dataframe according to search parameters
+df_venues = df_venues[df_venues["name"].str.contains(SEARCH_TERMS.upper())]
+df_venues = df_venues[df_venues["name"].str.contains(DROP_TERMS.upper()) == False]
+df_venues = df_venues[df_venues["station"] <= MAX_DIST_TO_STATION]
+df_venues = df_venues[df_venues["airport"] <= MAX_DIST_TO_AIRPORT]
+print(df_venues.shape[0], "venues found")
 
 # generate map in browser
-fig = px.scatter_mapbox(data_frame=df_filtered, 
+fig = px.scatter_mapbox(data_frame=df_venues, 
                         lat="latitude", 
                         lon="longitude", 
                         text="name",
-                        hover_data="phone", 
+                        hover_data=["phone", "station", "airport"],
                         color_discrete_sequence=["fuchsia"],
                         zoom=5, 
                         height=600)
